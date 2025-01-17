d21380aed158986e7433699f8ac34cfc79acfa24
LOL_toulan <lol_toulan@163.com>
2025-01-17 19:45:04 +0800

    feat(InteractiveSse): 优化 SSE 连接管理并添加交互功能
    - 重构组件以支持断开和重新建立 SSE 连接
    - 添加本地存储以保存用户 ID 和事件记录
    - 实现断开连接和重新连接的功能
    - 优化事件处理逻辑，确保正确接收和显示事件数据
    - 调整样式，增加按钮容器和自定义按钮样式

--- src/views/InteractiveSse.vue
+++ src/views/InteractiveSse.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <h1>Interactive SSE By Flux</h1>
+    <h1>Interactive SSE By Input</h1>
     <input
       type="text"
+      disabled
       v-model="userId"
       placeholder="please input userId"
     />
@@ -12,6 +13,14 @@
       @keyup="sendRequest"
       placeholder="Type something..."
     />
+    <div class="button-container">
+      <button @click="breakConnection" class="custom-button">
+        Break Connection
+      </button>
+      <button @click="reconnect" class="custom-button">
+        Reconnect
+      </button>
+    </div>
     <div v-if="events.length">
       <h2>Events:</h2>
       <ul>
@@ -31,31 +40,68 @@ export default {
   name: 'InteractiveSse',
   data() {
     return {
-      events: [],
+      events: JSON.parse(localStorage.getItem('events')) || [],
       inputValue: '',
-      userId: 'loltoulan',
+      userId: localStorage.getItem('userId') || 'loltoulan',
+      close: false,
+      eventSource: null,
     };
+  },
+  watch: {
+    events: {
+      handler(newVal) {
+        localStorage.setItem('events', JSON.stringify(newVal));
+      },
+      deep: true,
+    },
+    userId(newVal) {
+      localStorage.setItem('userId', newVal);
+    },
   },
   mounted() {
-    const eventSource = new EventSource(`http://localhost:8080/sse/sse-emitter/redis/${this.userId}`);
-    eventSource.onmessage = (event) => {
-      const data = event.data + ' - ' + new Date().toLocaleString();
-      this.events.push(data);
-    };
-
-    eventSource.onerror = () => {
-      console.error('Error occurred in SSE connection');
-      eventSource.close();
-    };
+    this.initEventSource();
+  },
+  beforeUnmount() {
+    if (this.eventSource) {
+      this.eventSource.close();
+    }
   },
   methods: {
+    initEventSource() {
+      if (this.eventSource) {
+        this.eventSource.close();
+      }
+      this.eventSource = new EventSource(`http://localhost:8080/sse/sse-emitter/redis/${this.userId}`);
+      this.eventSource.onmessage = (event) => {
+        console.log('Received event:', event.data);
+        const data = event.data + ' - ' + new Date().toLocaleString();
+        if (event == null) {
+          console.log('event is null');
+        } else {
+          this.events.push(data);
+        }
+      };
+
+      this.eventSource.onerror = () => {
+        console.error('Error occurred in SSE connection');
+        if (this.close) {
+          console.log('SSE connection closed.');
+          this.eventSource.close();
+        }
+      };
+    },
     sendRequest() {
+      if (this.eventSource.readyState === 2) {
+        console.log('SSE connection closed.');
+        this.eventSource.close();
+      }
       if (this.inputValue.trim() === '') return;
       if (this.userId.trim() === '') return;
 
       fetch(`http://localhost:8080/sse/sse-emitter/mock/${this.userId}?data=${encodeURIComponent(this.inputValue)}`)
         .then(response => {
           console.log('Response status:', response);
+          // return response.json(); // 解析响应数据
         })
         .then(data => {
           console.log('Response from server:', data);
@@ -64,6 +110,32 @@ export default {
         .catch(error => {
           console.error('Error sending request:', error);
         });
+    },
+    breakConnection() {
+      fetch(`http://localhost:8080/sse/sse-emitter/redis/close/${this.userId}`)
+        .then(response => {
+          console.log('Response status:', response);
+          // return response.json(); // 解析响应数据
+        })
+        .then(data => {
+          console.log('Response from server:', data);
+          // 处理响应数据
+        })
+        .catch(error => {
+          console.error('Error sending request:', error);
+        });
+      this.events = [];
+      this.inputValue = '';
+      this.close = true;
+      console.log('SSE connection closed.');
+      if (this.eventSource) {
+        this.eventSource.close();
+      }
+    },
+    reconnect() {
+      this.initEventSource();
+      this.close = false;
+      console.log('SSE connection re-established.');
     },
   },
 };
@@ -80,5 +152,21 @@ input {
   width: 80%;
   border: 1px solid #ccc;
   border-radius: 5px;
+}
+
+.button-container {
+  display: flex;
+  flex-direction: column; /* 垂直排列 */
+  align-items: right; /* 水平居中 */
+}
+
+.custom-button {
+  background-color: #42b983;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-bottom: 10px; /* 添加此行以增加间距 */
 }
 </style>
